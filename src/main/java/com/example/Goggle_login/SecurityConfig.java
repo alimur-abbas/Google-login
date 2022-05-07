72d5717eeb96946fa03cc3c21ad45c956308def5
alimur-abbas <alimurabbas8658402111@gmail.com>
2022-05-07 14:49:58 +0530

    latest code

--- src/main/java/com/example/Goggle_login/SecurityConfig.java
+++ src/main/java/com/example/Goggle_login/SecurityConfig.java
@@ -6,9 +6,14 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    ClientRegistrationRepository clientRegistrationRepository;
     @Autowired
     private CustomOidcUserService customOidcUserService;
     @Override
@@ -19,7 +24,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .oauth2Login().permitAll()
                 .and().
-                logout()
-                .logoutSuccessUrl("/").and().oauth2Login().userInfoEndpoint().oidcUserService(customOidcUserService);
+                logout().logoutSuccessUrl("/login").invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID").clearAuthentication(true);
+
+
+
+
+
+
+//
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/","/index.html").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .logout()
+//                .logoutSuccessHandler(oidcLogoutSuccessHandler())
+//                .invalidateHttpSession(true)
+//                .clearAuthentication(true)
+//                .deleteCookies("JSESSIONID").and().oauth2Login();
+
+
+
+
+//        http.csrf().disable().antMatcher("/**").authorizeRequests()
+//                .antMatchers("/", "/index.html").authenticated()
+//                .anyRequest().authenticated()
+//                .and()
+//                .oauth2Login().permitAll()
+//                .and().
+//                logout()
+//                .logoutSuccessUrl("/").and().oauth2Login().userInfoEndpoint().oidcUserService(customOidcUserService);
+    }
+
+
+
+
+
+
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8080/login");
+        return successHandler;
     }
 }

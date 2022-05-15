dc674c2ff776aa65d2009a9dd9dee6d36a5bb79b
alimur-abbas <alimurabbas8658402111@gmail.com>
2022-05-15 13:56:51 +0530

    integration of mcq_model with Google_auth

---
+++ src/main/java/com/example/Goggle_login/model/ExamType.java
@@ -0,0 +1,5 @@
+package com.example.Goggle_login.model;
+
+public enum ExamType {
+    RETAKE,ONCE_SCHEDULE_MINUTE,ONCE_SCHEDULE_DAYS
+}
---
+++ src/main/java/com/example/Goggle_login/model/QuestionModel.java
@@ -0,0 +1,74 @@
+package com.example.Goggle_login.model;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@NoArgsConstructor
+public class QuestionModel  {
+    private String questionId;
+    private String questionLabel;
+    private String questionDescription;
+    private String answerLabel;
+
+    public String getExaminationId() {
+        return examinationId;
+    }
+
+    public void setExaminationId(String examinationId) {
+        this.examinationId = examinationId;
+    }
+
+    List<OptionModel> optionModelList;
+    private String examinationId;
+
+    public String getQuestionLabel() {
+        return questionLabel;
+    }
+
+    public void setQuestionLabel(String questionLabel) {
+        this.questionLabel = questionLabel;
+    }
+
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuestionDescription() {
+        return questionDescription;
+    }
+
+    public void setQuestionDescription(String questionDescription) {
+        this.questionDescription = questionDescription;
+    }
+
+    public String getAnswerLabel() {
+        return answerLabel;
+    }
+
+    public void setAnswerLabel(String answerLabel) {
+        this.answerLabel = answerLabel;
+    }
+
+    public List<OptionModel> getOptionModelList() {
+        return optionModelList;
+    }
+
+    public void setOptionModelList(List<OptionModel> optionModelList) {
+        this.optionModelList = optionModelList;
+    }
+
+    @Override
+    public String toString() {
+        return "QuestionModel{" +
+                "questionId='" + questionId + '\'' +
+                ", questionLabel='" + questionLabel + '\'' +
+                ", questionDescription='" + questionDescription + '\'' +
+                ", answerLabel='" + answerLabel + '\'' +
+                ", optionModelList=" + optionModelList +
+                ", examinationId='" + examinationId + '\'' +
+                '}';
+    }
+}
